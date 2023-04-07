8bab6e62d2d22c31af4b126a51452535d0fc6a5a
Giannis Damilatis <johndamilatis786@gmail.com>
2023-04-07 14:42:30 +0300

    Almost done

--- api/get_character.py
+++ api/get_character.py
@@ -7,6 +7,47 @@ __version__ = '1.0.0'
 
 from requests import Session
 import sys
+import json
+from datetime import datetime
+import os
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+
+class CacheHandler(object):
+
+    def __init__(self):
+        self.file_path = 'cache/advancedData.json'
+        self.data = []
+        self.request = {
+            'requests': [],
+            'total_data': {
+                'total_requests': 0,
+                'total_succesful': 0,
+                'total_unsuccesful': 0,
+            }
+        }
+
+    def initialize_cache_data(self):
+        if os.path.isfile(self.file_path):
+            return
+        with open(self.file_path, 'w') as f:
+            json.dump(self.request, f)
+            return
+
+    def update_json(self, data):
+        print('data')
+        if os.path.isfile(self.file_path):
+            with open(self.file_path, 'r') as f:
+                cached_data = json.load(f)
+            cached_data['requests'].append(data)
+            cached_data['total_data']['total_requests'] += 1
+            if data['result'] == 'succesful':
+                cached_data['total_data']['total_succesful'] += 1
+            else:
+                cached_data['total_data']['total_unsuccesful'] += 1
+            with open(self.file_path, 'w') as f:
+                json.dump(cached_data, f)
 
 
 class Connector(object):
@@ -14,14 +55,34 @@ class Connector(object):
     def __init__(self, session, url, params):
         self.session = session
         self.url = url
-        self.characters = None
+        self.characters = {
+            'data': None,
+            'cached': None,
+        }
         self.params = params
 
+    def fetch_data(self, json_cache: str):
+        try:
+            with open('cache/'+json_cache, 'r') as f:
+                self.characters = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print('no local cache found', e)
+            self.characters = {
+                'data': None,
+                'cached': None,
+            }
+
     def connect(self):
+        self.fetch_data('characters.json')
+        if self.characters['data']:
+            return True
         response = self.session.get(self.url, params=self.params)
         if response.status_code == 200 or response.status_code == 304:
-            self.characters = response.json().get('results')
-
+            self.characters['data'] = response.json().get('results')
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.characters['cached'] = current_time
+            with open('cache/characters.json', 'w') as f:
+                json.dump(self.characters, f)
             return True
         else:
             return False
@@ -29,16 +90,50 @@ class Connector(object):
 
 class classHomeworld(object):
 
-    def __init__(self, session, url, params):
+    def __init__(self, session, url):
         self.session = session
         self.url = url
         self.homeworld = None
-        self.params = params
         self.earth_orbital_period = 365.25
         self.earth_rotation_period = 24
+        self.world = {
+            'name': None,
+            'population': None,
+            'orbital_period': None,
+            'rotation_period': None,
+            'correlation_day': None,
+            'correlation_year': None,
+        }
+
+    def worlds_correlation(self, day, year):
+        if day == 'unknown' or year == 'unknown':
+            self.world['correlation_day'] = 'unknown'
+            self.world['correlation_year'] = 'unknown'
+            return
+        self.world['correlation_day'] = round(
+            int(day) / self.earth_rotation_period, 2)
+        self.world['correlation_year'] = round(
+            int(year) / self.earth_orbital_period, 2)
+        return
 
     def connect(self):
-        pass
+        response = self.session.get(self.url)
+        if response.status_code == 200 or response.status_code == 304:
+            self.homeworld_data = response.json().get('result').get('properties')
+            if not self.homeworld_data:
+                return True
+            self.world['name'] = self.homeworld_data.get('name')
+            self.world['population'] = self.homeworld_data.get('population')
+            self.world['orbital_period'] = self.homeworld_data.get(
+                'orbital_period')
+            self.world['rotation_period'] = self.homeworld_data.get(
+                'rotation_period')
+
+            self.worlds_correlation(
+                self.world['rotation_period'], self.world['orbital_period'])
+            return True
+        else:
+            return False
 
 
 class SearchCharacter(object):
@@ -46,39 +141,94 @@ class SearchCharacter(object):
         self.session = session
         self.worldFlag = worldFlag
         self.url = url
-        self.data = None
+        self.world_data = None
         self.character = {
             'name': None,
             'height': None,
             'mass': None,
             'birth_year': None,
+            'world_url': None,
             'world': None,
             'population': None,
-            'correlation': None,
+            'correlation': {
+                'day': None,
+                'year': None,
+            },
+            'world_flag': worldFlag,
+            'cached': None
         }
         self.params = params
+        self.cache_values = {
+            'search': None,
+            'result': None,
+            'time': None,
+        }
+
+    def fetch_data(self, json_cache: str):
+        try:
+            with open('cache/'+json_cache, 'r') as f:
+                self.character = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print('no local cache found', e)
 
     def parseData(self, data, worldFlag):
+        data = data[0].get('properties')
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.character['cached'] = current_time
         if (worldFlag):
-            data = data.json().get('result')[0].get('properties')
             self.character['name'] = data.get('name')
             self.character['height'] = data.get('height')
             self.character['mass'] = data.get('mass')
             self.character['birth_year'] = data.get('birth_year')
+            self.character['world_url'] = data.get('homeworld')
+
+            connector = classHomeworld(
+                self.session, self.character['world_url'])
+            if not connector.connect():
+                raise Exception('Cannot connect to the API for the worlds')
+
+            self.world_data = getattr(connector, 'world')
+            self.character['world'] = self.world_data.get('name')
+            self.character['population'] = self.world_data.get('population')
+            self.character['correlation']['day'] = self.world_data.get(
+                'correlation_day')
+            self.character['correlation']['year'] = self.world_data.get(
+                'correlation_year')
+            return self.character
+        else:
+            self.character['name'] = data.get('name')
+            self.character['height'] = data.get('height')
+            self.character['mass'] = data.get('mass')
+            self.character['birth_year'] = data.get('birth_year')
+            return self.character
 
     def connect(self):
+        self.fetch_data(self.params['name']+'.json')
+        if self.character['cached']:
+            self.data = self.character
+            return True
+        cacheHandler = CacheHandler()
+
         response = self.session.get(self.url, params=self.params)
         if response.status_code == 200 or response.status_code == 304:
             self.data = response.json().get('result')
             if not self.data:
+                self.cache_values['search'] = self.params['name']
+                self.cache_values['result'] = 'Unsuccessful'
+                self.cache_values['time'] = datetime.now().strftime(
+                    "%Y-%m-%d %H:%M:%S")
+                cacheHandler.update_json(self.cache_values)
                 return True
-            if self.data.len() > 0:
+            if len(self.data) > 0:
                 self.data = self.parseData(self.data, self.worldFlag)
-            # self.data = response.json().get('result')[0].get('properties')
-            # self.character['name'] = self.data.get('name')
-            # self.character['height'] = self.data.get('height')
-            # self.character['mass'] = self.data.get('mass')
-            # self.character['birth_year'] = self.data.get('birth_year')
+                self.cache_values['search'] = self.params['name']
+                self.cache_values['result'] = 'Successful'
+                self.cache_values['time'] = datetime.now().strftime(
+                    "%Y-%m-%d %H:%M:%S")
+                cacheHandler.update_json(self.cache_values)
+                filename = 'cache/'+self.params['name']+'.json'
+                with open(filename, 'w') as f:
+                    json.dump(self.character, f)
             return True
         else:
             return False
@@ -86,10 +236,14 @@ class SearchCharacter(object):
 
 class Character(object):
 
-    def __init__(self, name, world):
+    def __init__(self, name=None, world=False, cache=False):
         self.name = name
         self.world = world
+        self.cache = cache
         self.session = Session()
+        # self.session.mount(
+        #     'http://', requests_cache.CachedSession('demo_cache', expire_after=requests_cache.timedelta(hours=1)))
+
         self.base_url = 'https://www.swapi.tech/api/people'
         self.session.headers = {
             'authority': 'www.swapi.tech',
@@ -126,15 +280,32 @@ class Character(object):
         self.characters = None
         self.character = None
 
+    def remove_cache(self):
+        folder_path = "cache"
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                    print(f"{file_path} deleted successfully")
+            except Exception as e:
+                print(f"Error deleting {file_path}: {e}")
+                exit()
+
     def main(self):
+        if self.cache:
+            self.remove_cache()
+            exit()
+        cacheHandler = CacheHandler()
+        cacheHandler.initialize_cache_data()
+        # exit()
         connector = Connector(self.session, self.base_url,
                               self.find_all_params)
         if not connector.connect():
             raise Exception('Cannot connect to the API')
         self.characters = getattr(connector, 'characters')
-        if not self.characters:
+        if not self.characters['data']:
             raise Exception('No characters found')
-        # print(self.characters)
 
         connector = SearchCharacter(self.session, self.base_url,
                                     self.params, self.world)
@@ -145,12 +316,27 @@ class Character(object):
         if not self.character_data:
             print('The force is not strong within you')
             sys.exit()
-        if self.character_data:
+        if self.character_data and self.character_data.get('world_flag'):
             print("Name: ", self.character['name'])
             print("Height: ", self.character['height'])
             print("Mass: ", self.character['mass'])
             print("Birth Year: ", self.character['birth_year'])
-
-# if __name__ == '__main__':
-#     character = Character(self, name)
-#     character.main()
+            print('                                  ')
+            print("Homeworld")
+            print("----------------------------------")
+            print("Name: ", self.character['world'])
+            print("Population: ", self.character['population'])
+            print('                                  ')
+            print('On', self.character['world'], '1 year on earth is', self.character['correlation']
+                  ['year'], 'years and 1 day ', self.character['correlation']['day'], 'days')
+            print('                                  ')
+            if self.character.get('cached'):
+                print('cached: ', self.character['cached'])
+        else:
+            print("Name: ", self.character['name'])
+            print("Height: ", self.character['height'])
+            print("Mass: ", self.character['mass'])
+            print("Birth Year: ", self.character['birth_year'])
+            print('                                  ')
+            if self.characterget('cached'):
+                print('cached: ', self.character['cached'])
--- manage-tasks.py
+++ manage-tasks.py
@@ -6,18 +6,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="A command-line interface to manage tasks")
 
-    # Define the available tasks as sub-commands
     subparsers = parser.add_subparsers(title="Available Tasks")
 
-    # Task 1
     parser.add_argument('--search', type=str,
                         help='Find a star wars character.')
 
-    # Task 2
     parser.add_argument('--world', action='store_true',
                         help='Find a star wars character and correlate their worlds to earth.')
 
+    parser.add_argument('--clean', action='store_true',
+                        help='Clean cache')
+
     # Parse the command-line arguments and execute the selected task
     args = parser.parse_args()
-    task1 = api.get_character.Character(args.search, args.world).main()
+    task1 = api.get_character.Character(
+        args.search, args.world, args.clean).main()
     # args.func()
